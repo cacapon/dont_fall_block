7f08ce994464e911375bd9d5fc7a95b632ae4fb1
Cacapon Project <cacapon.project@gmail.com>
2025-06-22 23:38:25 +0900

    WINDOW変数修正

--- srcs/title_scene.py
+++ srcs/title_scene.py
@@ -1,7 +1,7 @@
 import pyxel
 from srcs.base_scene import BaseScene 
 from .pyxel_utils import text_center
-from .config import GAMEMODE, WINDOW_W, WINDOW_H
+from .config import GAMEMODE, WINDOW_SIZE
 
 
 class TitleScene(BaseScene):
@@ -13,7 +13,7 @@ class TitleScene(BaseScene):
 	# TODO
     def draw(self):
         title_pos = 60
-        text_center(WINDOW_W // 2, title_pos, "don't", 7)
-        text_center(WINDOW_W // 2, title_pos + 12, "fall", 7)
-        text_center(WINDOW_W // 2, title_pos + 24, "blocks", 7)
-        text_center(WINDOW_W // 2, WINDOW_H - 24, "PRESS SPACE KEY", 7)
+        text_center(WINDOW_SIZE.x // 2, title_pos, "don't", 7)
+        text_center(WINDOW_SIZE.x // 2, title_pos + 12, "fall", 7)
+        text_center(WINDOW_SIZE.x // 2, title_pos + 24, "blocks", 7)
+        text_center(WINDOW_SIZE.x // 2, WINDOW_SIZE.y - 24, "PRESS SPACE KEY", 7)
