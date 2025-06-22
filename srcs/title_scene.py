933fd998bdfdaec6abb77a812f124af62a430e78
Cacapon Project <cacapon.project@gmail.com>
2025-06-22 21:11:47 +0900

    create title scene

--- srcs/title_scene.py
+++ srcs/title_scene.py
@@ -1,10 +1,19 @@
+import pyxel
 from srcs.base_scene import BaseScene 
-from config import GAMEMODE
+from .pyxel_utils import text_center
+from .config import GAMEMODE, WINDOW_W, WINDOW_H
 
 
 class TitleScene(BaseScene):
     def update(self) -> GAMEMODE:
-        pass
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            return GAMEMODE.Game
+        return GAMEMODE.Title
 
+	# TODO
     def draw(self):
-        pass
+        title_pos = 60
+        text_center(WINDOW_W // 2, title_pos, "don't", 7)
+        text_center(WINDOW_W // 2, title_pos + 12, "fall", 7)
+        text_center(WINDOW_W // 2, title_pos + 24, "blocks", 7)
+        text_center(WINDOW_W // 2, WINDOW_H - 24, "PRESS SPACE KEY", 7)
